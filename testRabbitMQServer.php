b6bdf1845fef473658bde6e5390b8d1fe35ec19c
mikorod622 <mr94@njit.edu>
2025-03-05 02:26:01 -0500

    Update testRabbitMQServer.php

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -3,7 +3,7 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc'); // Includes the RabbitMQ Library
-require_once('mysqlconnect.php'); // Includes the database config
+// require_once('mysqlconnect.php'); // Includes the database config
 // require_once('populateDB.php');
 
 define('DB_HOST', '127.0.0.1');
