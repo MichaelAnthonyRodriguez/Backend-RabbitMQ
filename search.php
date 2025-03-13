d687ffeb0a00728d2fcf28ecb883570d65cbbca5
JVM3 <146955184+JoeMuszynski@users.noreply.github.com>
2025-03-13 12:10:43 -0400

    Create search.php

---
+++ search.php
@@ -0,0 +1,8 @@
+// Search for, browse, and see details on films:
+// When entering a search into an input, return:
+// select * from movies where (search term is in the movie title)
+// When selecting a genre of movie, return:
+// select * from movies where (genre == selected)
+<?php
+
+?>
