95dca893787962eb5fb25e3e350402e8e1ac2987
cmb84 <cmb84@njit.edu>
2025-03-13 11:45:27 -0400

    api adding

--- api/addfavorites.php
+++ api/addfavorites.php
@@ -3,10 +3,11 @@ require_once('vendor/autoload.php');
 
 $client = new \GuzzleHttp\Client();
 
-$response = $client->request('GET', 'https://api.themoviedb.org/3/authentication', [
+$response = $client->request('POST', 'https://api.themoviedb.org/3/account/21871794/favorite', [
   'headers' => [
     'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNzgyMWU0YzQxNDFhNWZiY2FhYzA3NzdhYWJiODc2MCIsIm5iZiI6MTc0MTYxNjkwMi4wODMwMDAyLCJzdWIiOiI2N2NlZjcwNjNjMjU0NDQ4ODJlMzFkZTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zHLr6Jcvpr8NdKd4xZvwcqpRhJpO-Y874oXFlP8gqPI',
     'accept' => 'application/json',
+    'content-type' => 'application/json',
   ],
 ]);
 
